2a1b95407aa5add939f68949d858f9ef8beee973
Dmitriy Yakovlev <1d3ddd@gmail.com>
2021-09-28 05:31:55 +0200

    added keyboard control slidshow

--- app.js
+++ app.js
@@ -1,33 +1,40 @@
-const upBtn = document.querySelector('.up-button')
-const downBtn = document.querySelector('.down-button')
-const sidebar = document.querySelector('.sidebar')
-const container = document.querySelector('.container')
-const mainSlide = document.querySelector('.main-slide')
+const upBtn = document.querySelector(".up-button");
+const downBtn = document.querySelector(".down-button");
+const sidebar = document.querySelector(".sidebar");
+const container = document.querySelector(".container");
+const mainSlide = document.querySelector(".main-slide");
 
-const slidesCount= mainSlide.querySelectorAll('div').length
-let activeSlideIndex = 0
+const slidesCount = mainSlide.querySelectorAll("div").length;
+let activeSlideIndex = 0;
 
 sidebar.style.top = `-${(slidesCount - 1) * 100}vh`;
 
-upBtn.addEventListener('click',()=>changeSlide('up'))
-downBtn.addEventListener('click',()=>changeSlide('down'))
+upBtn.addEventListener("click", () => changeSlide("up"));
+downBtn.addEventListener("click", () => changeSlide("down"));
 
+document.addEventListener("keydown", e => {
+  if (e.key === "ArrowUp" || e.key === "ArrowRight") {
+    changeSlide("up");
+  }
+  if (e.key === "ArrowDown" || e.key === "ArrowLeft") {
+    changeSlide("down");
+  }
+});
 
 function changeSlide(direction) {
-    if (direction === 'up') {
-        activeSlideIndex++
-        if (activeSlideIndex === slidesCount) {
-            activeSlideIndex = 0
-        }
-    } else if (direction === 'down') {
-            activeSlideIndex--
-            if (activeSlideIndex < 0) {
-                activeSlideIndex = slidesCount - 1;
-            }
-        }
-        const height = container.clientHeight
-        mainSlide.style.transform = `translateY(-${activeSlideIndex*height}px)`;
-        sidebar.style.transform = `translateY(${activeSlideIndex*height}px)`;
+  if (direction === "up") {
+    activeSlideIndex++;
+    if (activeSlideIndex === slidesCount) {
+      activeSlideIndex = 0;
     }
-
-  
+  }
+  if (direction === "down") {
+    activeSlideIndex--;
+    if (activeSlideIndex < 0) {
+      activeSlideIndex = slidesCount - 1;
+    }
+  }
+  const height = container.clientHeight;
+  mainSlide.style.transform = `translateY(-${activeSlideIndex * height}px)`;
+  sidebar.style.transform = `translateY(${activeSlideIndex * height}px)`;
+}
